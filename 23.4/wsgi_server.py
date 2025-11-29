1217abe624b875b0a5f98519b2549550b0990ae7
Ehsan <ehsantavakoli62@mail.ru>
2025-11-29 17:09:45 +0300

    Cleaned working tree and ready for CI/CD setup

--- 23.4/wsgi_server.py
+++
@@ -1,7 +0,0 @@
-# wsgi_server.py
-
-# این فایل فقط برای اشاره به برنامه WSGI در routes.py استفاده می‌شود
-from routes import app as application
-
-# در محیط واقعی، شما این فایل را با Gunicorn اجرا می‌کنید:
-# gunicorn wsgi_server:application -b 127.0.0.1:8000
--- 7.6/7.6 9 part_3_utils.py
+++
@@ -1,9 +0,0 @@
-# utils.py 
-
-
-
-import logging
-logger = logging.getLogger('utils')
-def example_func():
-    logger.debug("Debug log from utils.")
-    logger.warning("Warning log from utils.")
